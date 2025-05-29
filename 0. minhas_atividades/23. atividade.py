00dad5431a203238b487f0b5a076ecf717def7b5
KaiioS2 <kaiopinto833@gmail.com>
2025-05-29 17:03:16 +0000

    save

--- 0. minhas_atividades/23. atividade.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-os.system("clear") # Limpa o Terminal.
-
-media = 0
-for i in range(4):
-    numero = float(input("digite suas notas: "))
-    media += numero / 4
-print(f"Media: {media}")
